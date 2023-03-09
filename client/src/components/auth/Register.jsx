4cefffcc954b35e6044f2fef9e5d4dd2427d4a86
Revati <revati.v.bordawekar@gmail.com>
2023-03-08 17:47:45 -0800

    connected redux to react for Register

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,5 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { registerUser } from "../../actions/authActions";
+import classnames from "classnames";
 
 const Register = () => {
   const [user, setUser] = useState({
@@ -10,7 +14,13 @@ const Register = () => {
     errors: {}
   });
 
+
   const { name, email, password, password2, errors } = user;
+  useEffect(() => {
+    if (errors) {
+      setUser((prevState) => ({ ...prevState, errors }));
+    }
+  }, [errors]);
 
   const onChange = e => {
     setUser({ ...user, [e.target.id]: e.target.value });
@@ -24,7 +34,7 @@ const Register = () => {
       password,
       password2
     };
-    console.log(newUser);
+    registerUser(newUser)
   };
 
   return (
@@ -51,8 +61,12 @@ const Register = () => {
                 error={errors.name}
                 id="name"
                 type="text"
+                className={classnames("", {
+                  invalid: errors.name,
+                })}
               />
               <label htmlFor="name">Name</label>
+              <span className="red-text">{errors.name}</span>
             </div>
             <div className="input-field col s12">
               <input
@@ -61,8 +75,12 @@ const Register = () => {
                 error={errors.email}
                 id="email"
                 type="email"
+                className={classnames("", {
+                  invalid: errors.email,
+                })}
               />
               <label htmlFor="email">Email</label>
+              <span className="red-text">{errors.email}</span>
             </div>
             <div className="input-field col s12">
               <input
@@ -71,8 +89,12 @@ const Register = () => {
                 error={errors.password}
                 id="password"
                 type="password"
+                className={classnames("", {
+                  invalid: errors.password,
+                })}
               />
               <label htmlFor="password">Password</label>
+              <span className="red-text">{errors.password}</span>
             </div>
             <div className="input-field col s12">
               <input
@@ -81,8 +103,12 @@ const Register = () => {
                 error={errors.password2}
                 id="password2"
                 type="password"
+                className={classnames("", {
+                  invalid: errors.password2,
+                })}
               />
               <label htmlFor="password2">Confirm Password</label>
+              <span className="red-text">{errors.password2}</span>
             </div>
             <div className="col s12" style={{ paddingLeft: "11.250px" }}>
               <button
@@ -105,4 +131,16 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+export default connect(
+  mapStateToProps,
+  { registerUser }
+)(Register);

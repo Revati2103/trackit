2d251d95bd706b9bab4e0132c70a950ee51c0819
Revati <revati.v.bordawekar@gmail.com>
2023-03-11 06:20:24 -0800

    exchange public token

--- routes/plaidRoutes.js
+++ routes/plaidRoutes.js
@@ -26,6 +26,31 @@ const config = new Configuration({
   //Instantiate the Plaid client with the configuration
   const client = new PlaidApi(config);
 
+  //Creates a Link token and return it
+app.get("/api/create_link_token", async (req, res, next) => {
+  const tokenResponse = await client.linkTokenCreate({
+    user: { client_user_id: req.sessionID },
+    client_name: "Plaid's Tiny Quickstart",
+    language: "en",
+    products: ["auth"],
+    country_codes: ["US"],
+    redirect_uri: process.env.PLAID_SANDBOX_REDIRECT_URI,
+  });
+  res.json(tokenResponse.data);
+});
+
+// Exchanges the public token from Plaid Link for an access token
+app.post("/api/exchange_public_token", async (req, res, next) => {
+  const exchangeResponse = await client.itemPublicTokenExchange({
+    public_token: req.body.public_token,
+  });
+
+  // FOR DEMO PURPOSES ONLY
+  // Store access_token in DB instead of session storage
+  req.session.access_token = exchangeResponse.data.access_token;
+  res.json(true);
+});
+
   // Define the middleware to authenticate the request using passport
 const authenticate = passport.authenticate('jwt', { session: false });
 

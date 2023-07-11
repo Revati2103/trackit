921c3138df8cf24d3d5763daa940e77b2842a490
Revati <revati.v.bordawekar@gmail.com>
2023-07-11 10:25:56 -0700

    updated cors policy

--- server.js
+++ server.js
@@ -14,9 +14,6 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
-app.use(cors({
-  origin: 'https://trackit-backend-bhj0.onrender.com/'
-}));
 
 //Passport middleware .
 

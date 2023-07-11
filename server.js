ff97211bf5afb76d5a2f633c2b23b3edf6a5f377
Revati <revati.v.bordawekar@gmail.com>
2023-07-10 18:44:04 -0700

    updated cors

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
+app.use(cors({
+  origin: 'https://trackit-kohl.vercel.app/'
+}));
 
 //Passport middleware .
 

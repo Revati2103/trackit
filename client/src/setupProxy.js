0b0bfaf9d6687b6f2a04214b01324ed0e5481cbc
Revati <revati.v.bordawekar@gmail.com>
2023-07-11 21:22:42 -0700

    proxy setup

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,8 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      //target: 'http://localhost:5500', // Change to the URL of your server
-      target: 'https://trackit-backend-bhj0.onrender.com/',
+      target: 'http://localhost:5500', // Change to the URL of your server
       changeOrigin: true,
     })
   );
